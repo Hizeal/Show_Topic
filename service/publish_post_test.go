175d688b627b8f8ff9e98dc89f27143951e2b0a2
Strand Zhou <z15129030062@outlook.com>
2022-05-09 18:34:13 +0800

    first commit

---
+++ service/publish_post_test.go
@@ -0,0 +1,52 @@
+package service
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Hizeal/lesson_2/repository"
+	"github.com/Hizeal/lesson_2/util"
+)
+
+func TestMain(m *testing.M) {
+	if err := repository.Init(); err != nil {
+		os.Exit(1)
+	}
+	if err := util.InitLogger(); err != nil {
+		os.Exit(1)
+	}
+	m.Run()
+}
+
+func TestPublishPost(t *testing.T) {
+
+	type args struct {
+		topicId int64
+		userId  int64
+		content string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "测试发布回帖",
+			args: args{
+				topicId: 1,
+				userId:  2,
+				content: "再次回帖",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := PublishPost(tt.args.topicId, tt.args.userId, tt.args.content)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PublishPost() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
